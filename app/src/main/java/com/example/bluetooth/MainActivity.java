67cee6b57f60c35e4f394bd5e81270fd120e6367
carftorr <73551328+carftorr@users.noreply.github.com>
2020-12-10 17:23:44 -0500

    .

--- app/src/main/java/com/example/bluetooth/MainActivity.java
+++ app/src/main/java/com/example/bluetooth/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.bluetooth;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
 import android.view.View;
@@ -17,31 +16,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //enlazamos el obejto de la clase con el id del xml
+        //enlazamos el objeto de la clase con el id del xml
         imagen=findViewById(R.id.Imgblue);
         //instanciamos al adaptador con el estado actual del bluetooth
         adaptador=BluetoothAdapter.getDefaultAdapter();
-        //Consultamos si el adapador es igual a nulo entonces lo indicamos que muestre un toast
+        //consultamos si el adapador es igual a nulo entonces lo indicamos que muestre un toast
         if (adaptador==null){
             imagen.setVisibility(View.VISIBLE);
             Toast.makeText(this,"No se puede activar el Bluetooth",Toast.LENGTH_LONG).show();
         }
-        //si el if no cumple con la condicion entonces el adaptador esta activado
+        //si el if no cumple con la condición entonces el adaptador esta activado
         else{
             setimageblue(adaptador.isEnabled());}
     }
-    //este metodo es para cuando hagamos click en la imagen llamamos al metodo set estadoblue
+    //este método es para cuando hagamos click en la imagen llamamos al método set estadoblue
     public void click_imagen(View v){
         setestadoblue();
     }
 
-    //en este metodo consultado segun las condicionales si el adaptado esta activado entonces que me muestre las imegen correspondiente sino tambien me va mostrat otra imagen segun su estado
+    //en este método consultado segun las condicionales si el adaptador esta activado entonces que me muestre las imagen correspondiente sino también me va mostrar otra imagen según su estado
     public void setimageblue(boolean valor)
     {
         if (valor)imagen.setImageResource(R.drawable.blue_on);
         else imagen.setImageResource(R.drawable.blue_off);
     }
-    //consultamos el estado de la adaptador y si esta activado mostramos imagina llamando al metodo setimageblue y lo desactivamos y lo mismo con la sieguiente condicion
+    //consultamos el estado de la adaptador y si esta activado mostramos la imagen llamando al metodo setimageblue y lo desactivamos y lo mismo con la siguiente condición
     public void setestadoblue()
     {
         if(adaptador.isEnabled()){
